40fe5619927b20ab722d0cf5f03667e0be901328
= <matthewcb11@gmail.com>
2020-10-13 19:18:05 -0600

    Added .env file to for secret and mongo uri

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
+require('dotenv').config()
 module.exports = {
-  mongoURI: "YOUR_MONGO_URI_HERE",
-  secretOrKey: "secret"
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET
 };
